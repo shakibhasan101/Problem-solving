9e5c7745b4dcc2bf06831010b145ef6ff74c7500
shakibhasan101 <shakibhasan1040@gmail.com>
2025-07-10 21:53:17 +0600

    solved Arrival of the General

---
+++ CodeForces/Arrival of the General.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+ using namespace std;
+
+ int main()
+ {
+     int n;
+     cin>> n;
+     int a[n];
+
+     for(int i=0;i<n;i++)
+     {
+         cin>>a[i];
+     }
+
+     int Max =a[0], idxmax=0;
+     int Min = a[0], idxmin=0;
+
+     for (int i=1;i<n;i++)
+     {
+
+
+         if(Max<a[i])
+         {
+              Max= a[i];
+              idxmax= i;
+         }
+
+
+         if(Min>=a[i])
+         {
+             Min= a[i];
+             idxmin = i;
+         }
+
+     }
+
+     cout <<"idxmax=" <<idxmax<<endl;
+     cout <<"idxmin="<<idxmin<<endl;
+
+     if(idxmax<idxmin)
+        cout << idxmax+(n-1-idxmin)<<endl;
+     else
+        cout<< idxmax + (n-1-idxmin-1)<<endl;
+
+     return 0;
+ }
