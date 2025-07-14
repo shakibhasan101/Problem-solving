725bbfbc07cd1e4c86d1939a82347a215f7908e1
shakibhasan101 <shakibhasan1040@gmail.com>
2025-07-14 06:53:23 +0600

    easy problem vasya the hipster 581A

---
+++ CodeForces/Vasya the Hipster.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b;
+    cin>> a>>b;
+    int min_day = min (a,b);
+    int left_shocks;
+    if(a>b)
+        left_shocks = a-b;
+    else
+        left_shocks = b-a;
+
+    cout<<min_day<<" "<<left_shocks/2;
+    return 0;
+}
